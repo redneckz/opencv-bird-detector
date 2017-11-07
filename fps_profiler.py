36a60c0e6007b403181a8b8c5029e28aa5d9b71b
Alexander Alexandrov <aalexandrovv@gmail.com>
2017-11-07 15:50:00 +0300

    Fixed FPS profiler

--- fps_profiler.py
+++ fps_profiler.py
@@ -7,13 +7,12 @@ WINDOW_SIZE = DEFAULT_FPS * 10 # It is about half a minute at 60fps
 
 class FPSProfiler:
     def __init__(self):
-        self.currentFPS = 0
         self.measurements = np.empty(WINDOW_SIZE)
         self.measurements.fill(DEFAULT_FPS)
         self.pointer = 0
 
     def __iter__(self):
-        yield self.currentFPS
+        yield self.measurements[self.pointer - 1]
         yield self.measurements.min()
         yield self.measurements.mean()
         yield self.measurements.max()
@@ -22,7 +21,7 @@ class FPSProfiler:
         startTime = time()
         result = func()
         deltaTime = time() - startTime + DT_EPS
-        self.currentFPS = 1 / deltaTime
-        self.measurements[self.pointer] = self.currentFPS
+        currentFPS = 1 / deltaTime
+        self.measurements[self.pointer] = currentFPS
         self.pointer = (self.pointer + 1) % WINDOW_SIZE
         return result
